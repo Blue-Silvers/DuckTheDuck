c1e97883d3f4a29e926999821526f9e2099e9aa6
Skoys <royer.coppens@gmail.com>
2024-01-27 11:18:38 +0100

    [WIP]PlayerInputs

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -11,6 +12,8 @@ public class Player : MonoBehaviour
     public bool sameSensitivity = true;
     public bool FOV = false;
 
+    //public enum float{0, 1, 2, 3};
+
     public Camera cam;
 
     private Rigidbody rb;
@@ -22,17 +25,33 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Movement
         float Up_Down = Input.GetAxis("Vertical");
         float Left_Right = Input.GetAxis("Horizontal");
 
         Vector3 direction = (cam.transform.forward * Up_Down) + (cam.transform.right * Left_Right);
         rb.velocity = direction * speed;
 
+        //Camera
         if (sameSensitivity) { ySensitivity = xSensitivity; }
         if (FOV) { }
         float rotateHorizontal = Input.GetAxis("Mouse X");
         float rotateVertical = Input.GetAxis("Mouse Y");
         Vector3 rotation = new Vector3(rotateVertical * xSensitivity, -rotateHorizontal * ySensitivity, 0);
         cam.transform.eulerAngles -= rotation;
+
+        // interaction
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Hit: " + hit.collider.name);
+                Debug.Log("Distance: " + hit.distance);
+                Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow, 5f);
+                Debug.DrawLine(ray.origin, ray.direction * 10, Color.red, 5f);
+            }
+        }
     }
 }
